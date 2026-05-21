5b615970acdce4d6e38b5c6d7189654c21601eee
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-05-21 16:41:29 -0400

    const adapted to gus

--- lib/const.nix
+++ lib/const.nix
@@ -1,20 +1,20 @@
 let
-  user = "cristian";
+  user = "gus-nixos";
   homeDir = "/home/${user}";
 in
 {
   inherit user;
   inherit homeDir;
-  dotfilesDir = "${homeDir}/dotfiles";
+  dotfilesDir = "${homeDir}/gus-nixos";
   systemState = "25.11";
   homeState = "25.11";
   hostName = "wsl";
   timezone = "America/Caracas";
   systemLanguage = "en_US.UTF-8";
   parametersLanguage = "es_ES.UTF-8";
-  userDescription = "cristian hernandez";
+  userDescription = "gustavo alviarez";
   git = {
-    name = "cristian";
-    email = "129994548+icristianhernandez@users.noreply.github.com";
+    name = "gustavo";
+    email = "gusalv2001@gmail.com";
   };
 }
